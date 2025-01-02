e369c95f0723422394566c9a0cb5c1d6a487abae
Liochatchawan <chatchawan2548m@gmail.com>
2025-01-02 18:15:32 +0700

    Update main website

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import { useState } from 'react';
 export default function Home() {
   const [imageSrc, setImageSrc] = useState("/Lio.jpg"); // กำหนดรูปภาพเริ่มต้น
   const toggleImage = () => {
